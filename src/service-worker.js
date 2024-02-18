00a6d657ba64346a28daae668ebf509ea3475c6b
Kristian <70901103+Kris-github0@users.noreply.github.com>
2024-02-18 20:08:31 +0000

    Add extension icon click functionality

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,3 +7,25 @@ chrome.runtime.onMessage.addListener((message) => {
     }
   }
 });
+
+chrome.action.onClicked.addListener(async () => {
+  const tab = await getCurrentTab();
+  const regex =
+    /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)?([\w\-]+)?(\S+)?$/gm;
+
+  if (regex.test(tab.url)) {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL("./options/options.html"));
+    }
+  } else {
+  }
+});
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+
+  return tab;
+}
